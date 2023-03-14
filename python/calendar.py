3f48963dd308fd405d280ae1333292c7b94aba71
Chris <103456949+crsanf@users.noreply.github.com>
2023-03-14 19:12:08 -0400

    Update calendar.py
    Modified to better code

--- python/calendar.py
+++ python/calendar.py
@@ -1,22 +1,5 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 import calendar
 
-date_string = input()
-split_date = date_string.split(' ')
-month,day,year = [int(x) for x in split_date]
-day = calendar.weekday(year,month,day)
-
-if day == 0:
-    print("MONDAY")
-elif day == 1:
-    print("TUESDAY")
-elif day == 2:
-    print("WEDNESDAY")
-elif day == 3:
-    print("THURSDAY")
-elif day == 4:
-    print("FRIDAY")
-elif day == 5:
-    print("SATURDAY")
-elif day == 6:
-    print("SUNDAY")
+month,day,year = map(int,input().split(' '))
+day_index = calendar.weekday(year,month,day)
+print(calendar.day_name[day_index].upper())
