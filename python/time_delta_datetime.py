b52df49773f07d340eec56fd906a5a31c8d12d05
Chris <103456949+crsanf@users.noreply.github.com>
2023-03-14 17:36:53 -0400

    Create time_delta_datetime.py

---
+++ python/time_delta_datetime.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    datetime_t1 = parse_datetime_string(t1)
+    datetime_t2 = parse_datetime_string(t2)
+    delta = datetime_t1 - datetime_t2
+    total_seconds = (delta.days * 24 * 60 * 60) + delta.seconds
+    print(total_seconds)
+
+def parse_datetime_string(datetime_string):
+    datetime_split = datetime_string.split(' ')
+    recompiled_datetime_string = '-'.join(datetime_split[1:])
+    format_pattern = '%d-%b-%Y-%X-%z'
+    datetime_tx = datetime.strptime(recompiled_datetime_string,format_pattern)
+    return datetime_tx
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(str(delta) + '\n')
+
+    fptr.close()
